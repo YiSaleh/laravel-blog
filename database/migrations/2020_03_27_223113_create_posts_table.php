106c423d4a0afdbec2481e3425be9b5d168404a4
YiSaleh <yara_ahmed_saleh@yahoo.com>
2020-03-28 18:25:02 +0200

    adding laravel files

---
+++ database/migrations/2020_03_27_223113_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id("id");
+            $table->timestamps();
+            $table->string("title");
+            $table->string("posted_by");
+        
+        });
+
+     
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
